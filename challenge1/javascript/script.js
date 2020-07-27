b818a77478ecc329d4a76d65363d73ff33f448bc
niharikamadke <t.madke@rediffmail.com>
2020-07-27 21:58:26 +0530

    Commiting previous file

---
+++ challenge1/javascript/script.js
@@ -0,0 +1,8 @@
+let myDiv = document.querySelector("#i7");
+
+myDiv.addEventListener( "mouseover" , () => {
+  myDiv.style.backgroundImage = 'url('unpkg.com/ionicons@5.1.2/dist/svg/arrow-redo-circle.svg')';
+});
+myDiv.addEventListener( "mouseleave" , () => {
+  myDiv.style.backgroundImage = 'url('C:\Users\HP\Desktop\challenges\challenge1\images\icon-share.svg')';
+});
